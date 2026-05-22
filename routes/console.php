9991a3b41aa6cf48b4b07d0cd893487df141a3ee
Minh Le <ducminhldm@gmail.com>
2026-05-22 14:11:32 +0700

    feat(schedule): daily cleanup of ip_view_logs > 30 days

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    \App\Models\IpViewLog::where('viewed_at', '<', now()->subDays(30))->delete();
+})->daily()->name('cleanup-ip-view-logs');
